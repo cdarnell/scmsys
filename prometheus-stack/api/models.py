4c7e2ae60e511dd2c999498cb2f2a1521741312f
gentoofoo <charlesadarnell@gmail.com>
2026-03-01 11:04:15 -0500

    chore(repo): flatten layout and add home stacks

--- prometheus-stack/api/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-class Vehicle(BaseModel):
-    id: int
-    vin: Optional[str]
-    display_name: Optional[str]
-    state: Optional[str]
